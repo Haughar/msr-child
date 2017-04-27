d39b96f7f989c461affc1749431862ed86755dd7
Ali Haugh <haughar@uw.edu>
2017-04-26 22:54:11 -0700

    Show all fundraisers on a campaign's page.

--- single-campaign.php
+++ single-campaign.php
@@ -8,7 +8,8 @@
 
 get_header(); ?>
 
-<?php while ( have_posts() ) : the_post(); ?>
+<?php while ( have_posts() ) : the_post();
+	global $post_id; ?>
 	
 	<?php 
 		if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it. ?>
@@ -48,6 +49,72 @@ get_header(); ?>
 			</div>
 		<?php endif; ?>
 	
-<?php endwhile; // end of the loop. ?>
+<?php endwhile; // end of the loop. 
+
+$args = array(
+    'post_type' => 'fundraiser',
+    'meta_key' => 'fundraiser-campaign',
+    'meta_value' => $post_id
+);
+
+$post_query = new WP_Query($args);
+if($post_query->have_posts() ) {
+  while($post_query->have_posts() ) {
+    $post_query->the_post();
+    $post = get_post();
+    $id = $post->ID; ?>
+
+    <article id="post-<?php the_ID(); ?>" <?php post_class('post-grid'); ?>>
+	
+		<?php if ( has_post_thumbnail() ) { 
+		/** Normal container for posts with thumbnail */ ?>
+		
+		<?php $image_thumb_src = wp_get_attachment_image_src(get_post_thumbnail_id(), 'msr-post-grid-thumb');?>
+		
+		<a class="entry-thumb" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" style="background-image:url( <?php echo $image_thumb_src[0]; ?> );">
+			<div class="entry-thumb-icon"></div>
+		</a>
+
+		<div class="entry-text-content">
+			
+		<?php } else {
+		/** Added class for adjusting post size without thumbnail */ ?>
+		
+		<div class="entry-text-content no-thumb">
+		
+		<?php } ?>
+			
+			<header class="entry-header">
+				<?php the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
+		
+				<?php if ( 'post' == get_post_type() ) : ?>
+				<div class="entry-meta">
+					<?php the_time('M j, Y') ?> 
+				</div><!-- .entry-meta -->
+				<?php endif; ?>
+			</header><!-- .entry-header -->
+		
+			<?php if(has_tag('feature', null)){ ?>
+				<div class="feature-content"><span>Feature</span></div>
+			<?php } ?>
+
+			<div class="entry-content">
+				<?php echo the_excerpt(); ?>
+			</div><!-- .entry-content -->
+		</div>
+		<div class="entry-more">
+			
+			<?php if ( has_post_format( 'video' )) { ?>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"> Watch Video</a>
+			<?php } else { ?>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php echo __('Read More', 'msr') ?></a>
+			<?php } ?>
+		</div>
+		
+	</article><!-- #post-## -->
+
+<?php  }
+}
+?>
 
 <?php get_footer(); ?>
